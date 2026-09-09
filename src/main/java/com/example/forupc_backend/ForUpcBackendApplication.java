ce610c915b522cda367bf085293597ee9b44b580
mati <matubarovero@gmail.com>
2026-09-09 20:38:45 -0300

    primer commit

---
+++ src/main/java/com/example/forupc_backend/ForUpcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.forupc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForUpcBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForUpcBackendApplication.class, args);
+    }
+
+}
